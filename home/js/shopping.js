e3d440fbebb64a2c1e14481b0788b5306ac51dec
RyElijah <rsluar@student.apc.edu.ph>
2025-01-12 20:37:30 +0800

    update index.html

--- home/js/shopping.js
+++ home/js/shopping.js
@@ -1,14 +1,28 @@
-const app3 = Vue.createApp({
+const app = Vue.createApp({
     data() {
       return {
-        itemName: null,
+        itemType: '',
+        itemName: '',
+        itemUrl: '',
         itemNumber: null,
-        itemImportant: false,
-        shoppingList: [
-          { name: 'Tomatoes', number: 5, important: false, found: false },
-          { name: 'Bread', number: 1, important: false, found: false },
-          { name: 'Soap', number: 1, important: true, found: true }
-        ]
+        preDefItems: [
+          { name: 'Burrito', type: 'Dinner', imgUrl: 'img_burrito.svg' },
+          { name: 'Pizza', type: 'Dinner', imgUrl: 'img_pizza.svg' },
+          { name: 'Pho Soup', type: 'Dinner', imgUrl: 'img_soup.svg' },
+          { name: 'Spaghetti', type: 'Dinner', imgUrl: 'img_spaghetti.svg' },
+          { name: 'Fish', type: 'Dinner', imgUrl: 'img_fish.svg' },
+          { name: 'Cake', type: 'Dessert', imgUrl: 'img_cake.svg' },
+          { name: 'Rice', type: 'Dinner', imgUrl: 'img_rice.svg' },
+          { name: 'Salad', type: 'Dinner', imgUrl: 'img_salad.svg' },
+          { name: 'Coke', type: 'Drink', imgUrl: 'img_soda.svg' },
+          { name: 'Green Soda', type: 'Drink', imgUrl: 'img_greenSoda.svg' },
+          { name: 'Doughnut', type: 'Dessert', imgUrl: 'img_doughnut.svg' },
+          { name: 'Ice Cream', type: 'Dessert', imgUrl: 'img_iceCream.svg' },
+          { name: 'Lemonade', type: 'Drink', imgUrl: 'img_lemonade.svg' },
+          { name: 'Pancakes', type: 'Dessert', imgUrl: 'img_pancakes.svg' },
+          { name: 'Water', type: 'Drink', imgUrl: 'img_water.svg' }
+        ],
+        order: []
       }
     },
     methods: {
@@ -16,14 +30,17 @@ const app3 = Vue.createApp({
         let item = {
           name: this.itemName,
           number: this.itemNumber,
-          important: this.itemImportant,
-          found: false
+          url: this.itemUrl
         }
-        this.shoppingList.push(item)
-        this.itemName = null
+        this.order.push(item)
+        this.itemType = ''
+        this.itemName = ''
         this.itemNumber = null  
-        this.itemImportant = false
+        this.itemUrl = ''
+      },
+      newUrl(e) {
+        this.itemUrl = e.target.options[e.target.selectedIndex].getAttribute("data-url")
       }
     }
   })
-app3.mount('#app3')
+ app.mount('#app')

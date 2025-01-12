f969ce3fc43070aa11ef2d7f81861d46802bb9dc
RyElijah <rsluar@student.apc.edu.ph>
2025-01-12 20:59:00 +0800

    update again

--- home/js/shopping.js
+++ home/js/shopping.js
@@ -1,28 +1,14 @@
 const app = Vue.createApp({
     data() {
       return {
-        itemType: '',
-        itemName: '',
-        itemUrl: '',
+        itemName: null,
         itemNumber: null,
-        preDefItems: [
-          { name: 'Burrito', type: 'Dinner', imgUrl: 'img_burrito.svg' },
-          { name: 'Pizza', type: 'Dinner', imgUrl: 'img_pizza.svg' },
-          { name: 'Pho Soup', type: 'Dinner', imgUrl: 'img_soup.svg' },
-          { name: 'Spaghetti', type: 'Dinner', imgUrl: 'img_spaghetti.svg' },
-          { name: 'Fish', type: 'Dinner', imgUrl: 'img_fish.svg' },
-          { name: 'Cake', type: 'Dessert', imgUrl: 'img_cake.svg' },
-          { name: 'Rice', type: 'Dinner', imgUrl: 'img_rice.svg' },
-          { name: 'Salad', type: 'Dinner', imgUrl: 'img_salad.svg' },
-          { name: 'Coke', type: 'Drink', imgUrl: 'img_soda.svg' },
-          { name: 'Green Soda', type: 'Drink', imgUrl: 'img_greenSoda.svg' },
-          { name: 'Doughnut', type: 'Dessert', imgUrl: 'img_doughnut.svg' },
-          { name: 'Ice Cream', type: 'Dessert', imgUrl: 'img_iceCream.svg' },
-          { name: 'Lemonade', type: 'Drink', imgUrl: 'img_lemonade.svg' },
-          { name: 'Pancakes', type: 'Dessert', imgUrl: 'img_pancakes.svg' },
-          { name: 'Water', type: 'Drink', imgUrl: 'img_water.svg' }
-        ],
-        order: []
+        itemImportant: false,
+        shoppingList: [
+          { name: 'Tomatoes', number: 5, important: false, found: false },
+          { name: 'Bread', number: 1, important: false, found: false },
+          { name: 'Soap', number: 1, important: true, found: true }
+        ]
       }
     },
     methods: {
@@ -30,16 +16,13 @@ const app = Vue.createApp({
         let item = {
           name: this.itemName,
           number: this.itemNumber,
-          url: this.itemUrl
+          important: this.itemImportant,
+          found: false
         }
-        this.order.push(item)
-        this.itemType = ''
-        this.itemName = ''
+        this.shoppingList.push(item)
+        this.itemName = null
         this.itemNumber = null  
-        this.itemUrl = ''
-      },
-      newUrl(e) {
-        this.itemUrl = e.target.options[e.target.selectedIndex].getAttribute("data-url")
+        this.itemImportant = false
       }
     }
   })

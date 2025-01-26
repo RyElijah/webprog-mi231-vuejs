cd84b64b4d3d711d6adc23319c5afe6b0211df79
RyElijah <rsluar@student.apc.edu.ph>
2025-01-27 00:21:40 +0800

    updated food for sandwiches

--- vue-project/src/components/FoodItem2.vue
+++ vue-project/src/components/FoodItem2.vue
@@ -9,8 +9,8 @@
 export default {
     data() {
         return {
-            name: 'Apples',
-            message: 'I like apples'
+            name: 'Mangoes',
+            message: 'I like mangoes'
         }
     }
 };
@@ -22,7 +22,5 @@ export default {
           display: inline-block;
           margin: 10px;
           padding: 10px;
-          background-color: lightgreen;
-      }
-      
-      </style>  
+          background-color: lightyellow;
+      }</style>  
